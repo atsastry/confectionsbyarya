8922395124a67d8d573f51b981d7751d88b6bbdc
Arya Sastry <atsastry@calpoly.edu>
2024-01-03 13:44:41 -0800

    edited About page again

--- frontend/src/About.tsx
+++ frontend/src/About.tsx
@@ -5,7 +5,7 @@ import {Link, useParams} from "react-router-dom";
 function About() {
     return (
         <div className="container">
-            <p>Hello there! I'm Arya Sastry, and I'm thrilled to welcome you to my baking world. Thank you for taking the time to explore my page. Baking has been my passion since the age of 6, and it has evolved into a source of joy, creativity, and therapy for me. There's something incredibly fulfilling about the art of baking—it's not just a skill, but a way to bring people together. I created this website to organize my recipes efficiently, and I'm currently in the process of expanding it into a platform where customers can easily order desserts and make payments. This venture seamlessly combines my love for coding and baking. I hope you enjoy navigating through my website and discover something truly delightful that captures your palate! <3 </p>
+            <p> Hello there! I'm Arya Sastry, and I'm thrilled to welcome you to my baking world. Thank you for taking the time to explore my page. Baking has been my passion since the age of 6, and it has evolved into a source of joy, creativity, and therapy for me. There's something incredibly fulfilling about the art of baking—it's not just a skill, but a way to bring people together. I created this website to organize my recipes efficiently, and I'm currently in the process of expanding it into a platform where customers can easily order desserts and make payments. This venture seamlessly combines my love for coding and baking. I hope you enjoy navigating through my website and discover something tasty that catches your eye! </p>
         </div>
     );
 }
