229f4f022f4fb510971a9f56773ec25685cc25f7
Arya Sastry <atsastry@calpoly.edu>
2024-01-04 17:31:52 -0800

    file not found catching

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,18 @@ import RecipePreview from './components/RecipePreview.tsx';
 // import RecipeData from "./RecipeData.json";
 import Home from "./Home.js";
 import Recipe from "./Recipe.tsx";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation} from 'react-router-dom';
 import Nav from "./Nav.tsx";
 import About from "./About.tsx";
 
+function NotFound () {
+  let location = useLocation();
+  return (
+    <div>
+      Cannot find file {location.pathname}.
+    </div>
+  ); 
+}
 
 function App() {
 
@@ -20,6 +28,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/recipe/:name" element={<Recipe />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
